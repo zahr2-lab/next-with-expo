c41ecfaad55e9743f32ea92182d07e843f2756fb
zahr2-lab <69382928+zahr2-lab@users.noreply.github.com>
2021-12-07 00:12:05 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+// @generated: @expo/next-adapter@2.1.5
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to Expo + Next.js</Text>
+      <Text>Would you like to buy a car?</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    fontSize: 16
+  }
+});
